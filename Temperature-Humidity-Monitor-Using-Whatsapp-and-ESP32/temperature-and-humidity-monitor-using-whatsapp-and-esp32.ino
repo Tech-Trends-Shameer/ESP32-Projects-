9f87021ab7ae6b97520e16a7e38f386efab751f5
shameermohamed <shameer50@gmail.com>
2023-02-01 15:16:15 +0530

    Create temperature-and-humidity-monitor-using-whatsapp-and-esp32.ino

---
+++ Temperature-Humidity-Monitor-Using-Whatsapp-and-ESP32/temperature-and-humidity-monitor-using-whatsapp-and-esp32.ino
@@ -0,0 +1,68 @@
+//Tech Trends Shameer
+//Temperature Monitor Using Whatsapp
+
+#include <WiFi.h> 
+#include <ThingESP.h>
+#include <DHT.h>
+
+#define DHTPIN 5 
+#define DHTTYPE DHT11     // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+ThingESP32 thing("Username", "ProjectName", "ProjectUserName");
+
+unsigned long previousMillis = 0;
+const long INTERVAL = 6000;  
+
+void setup()
+{
+  Serial.begin(115200);
+
+ // pinMode(LED, OUTPUT);
+
+  thing.SetWiFi("WiFiUsername", "WiFiPassword");
+
+  thing.initDevice();
+
+}
+
+
+String HandleResponse(String query)
+{
+
+ float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+  Serial.println("Temperature :");
+  Serial.print(t);
+  Serial.println("Humidity");
+  Serial.print(h);
+  String humid = "Humidity: " + String(h) + " % \n";
+  String temp = "Temperature: " + String(t) + "°C, " + String(f) + "°F \n";
+
+  
+  if (query == "temperature") {
+   
+    return temp;
+  }
+
+  else if (query == "humidity") {
+  
+    return humid;
+  }
+ 
+  else return "Your query was invalid..";
+
+}
+
+
+void loop()
+{ 
+
+  thing.Handle();
+
+}
+ 
+ 
