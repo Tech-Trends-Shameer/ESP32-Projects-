d1c191aaf854f6c5def2528388998306b1efbfff
shameermohamed <shameer50@gmail.com>
2025-01-19 16:03:38 +0530

    Create calibration.ino

---
+++ Environment-Monitoring-System-with-Capacitive-Soil-Sensor-ESP32-and-Blynk-IoT/calibration.ino
@@ -0,0 +1,13 @@
+const int soilMoisturePin = A0; // Analog pin connected to the sensor
+
+void setup() {
+  Serial.begin(115200); // Initialize Serial Monitor
+  Serial.println("Reading raw soil moisture sensor values...");
+}
+
+void loop() {
+  int sensorValue = analogRead(soilMoisturePin); // Read analog value
+  Serial.print("Sensor Value: ");
+  Serial.println(sensorValue); // Print the raw value
+  delay(1000); // Wait for 1 second
+}
